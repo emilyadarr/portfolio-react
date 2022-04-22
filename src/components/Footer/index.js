de1d912b38f27f797580f54f215628c11407b7c1
Emily <emilyadarr@gmail.com>
2022-04-22 09:09:18 -0400

    add footer component and begin project component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">Created by Emily Darr</div>
+    </footer>
+  );
+};
+
+export default Footer;
