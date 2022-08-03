15c7de85eacdf89db49edf5a6b4a524da2e6822d
Emily <emilyadarr@gmail.com>
2022-08-03 14:38:07 -0400

    update to bio

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,7 +7,7 @@ function About() {
       <h3 id="about">About Me</h3>
       <div className="bio">
         <img src={headshot} className="headshot" alt='headshot' />
-        <p>Located in Indianapolis, IN currently enrolled in the Butler University Executive Education Coding Boot Camp. Graduated from the University of Indianapolis in 2015 with a BA in Business Administration and Communication with a focus in Public Relations. Work experience includes copywriting and editing, graphic design, media relations, social media management and more in multiple industries such as film, nonprofit, B2B tech and healthcare.</p>
+        <p>Located in Indianapolis, IN recently graduated from Butler University Executive Education Coding Boot Camp. Graduated from the University of Indianapolis in 2015 with a BA in Business Administration and Communication with a focus in Public Relations. Work experience includes copywriting and editing, graphic design, media relations, social media management and more in multiple industries such as film, nonprofit, B2B tech and healthcare.</p>
       </div>
     </section>
   );
